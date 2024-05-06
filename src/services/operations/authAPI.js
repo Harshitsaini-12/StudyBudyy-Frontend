d7a5baaefe3ee9a0d64e0da08563b494aa220e7e
Harshitsaini-12 <harshitsaini3582@gmail.com>
2024-05-06 21:11:46 +0530

    adding backend folder in same folder

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -19,6 +19,7 @@ export function sendOtp(email, navigate) {
     const toastId = toast.loading("Loading...")
     dispatch(setLoading(true))
     try {
+      console.log("inside tryu")
       const response = await apiConnector("POST", SENDOTP_API, {
         email,
         checkUserPresent: true,
